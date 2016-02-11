7e43260a0d0f1163f19de61236cad239b061fe14
hoge1e3 <hoge1e3@gmail.com>
2016-02-11 20:33:47 +0900

    waitFor use $.when

--- www/js/editor/FileMenu.js
+++ www/js/editor/FileMenu.js
@@ -27,7 +27,7 @@ define(function (require) {
         "use strict";
         var _this=this;
         
-        //$LASTPOS=3000033;//jseditor.FileMenu:33
+        //$LASTPOS=3000049;//jseditor.FileMenu:49
         Tonyu.extend(_this,p);
       },
       newFile :function _trc_FileMenu_newFile() {
@@ -40,45 +40,47 @@ define(function (require) {
         var nf;
         var isd;
         
-        //$LASTPOS=3000074;//jseditor.FileMenu:74
+        //$LASTPOS=3000090;//jseditor.FileMenu:90
         es = _this.editorSet;
         
-        //$LASTPOS=3000096;//jseditor.FileMenu:96
+        //$LASTPOS=3000112;//jseditor.FileMenu:112
         fl = _this.fileList;
         
-        //$LASTPOS=3000117;//jseditor.FileMenu:117
+        //$LASTPOS=3000133;//jseditor.FileMenu:133
         dir = fl.curDir;
         
-        //$LASTPOS=3000140;//jseditor.FileMenu:140
+        //$LASTPOS=3000156;//jseditor.FileMenu:156
         if (! dir) {
           return _this;
         }
-        //$LASTPOS=3000162;//jseditor.FileMenu:162
+        //$LASTPOS=3000178;//jseditor.FileMenu:178
         n = _this.waitFor(UIDiag.prompt("ファイル名"));
         
-        //$LASTPOS=3000205;//jseditor.FileMenu:205
+        //$LASTPOS=3000221;//jseditor.FileMenu:221
+        console.log("fn",n);
+        //$LASTPOS=3000246;//jseditor.FileMenu:246
         nf = dir.rel(n);
         
-        //$LASTPOS=3000228;//jseditor.FileMenu:228
+        //$LASTPOS=3000269;//jseditor.FileMenu:269
         isd = _this.waitFor(nf.isDir());
         
-        //$LASTPOS=3000261;//jseditor.FileMenu:261
+        //$LASTPOS=3000302;//jseditor.FileMenu:302
         if (isd) {
-          //$LASTPOS=3000280;//jseditor.FileMenu:280
+          //$LASTPOS=3000321;//jseditor.FileMenu:321
           dir=nf;
-          //$LASTPOS=3000296;//jseditor.FileMenu:296
+          //$LASTPOS=3000337;//jseditor.FileMenu:337
           _this.waitFor(nf.mkdir());
           
         } else {
-          //$LASTPOS=3000338;//jseditor.FileMenu:338
+          //$LASTPOS=3000379;//jseditor.FileMenu:379
           _this.waitFor(nf.text(""));
           
         }
-        //$LASTPOS=3000370;//jseditor.FileMenu:370
+        //$LASTPOS=3000411;//jseditor.FileMenu:411
         _this.waitFor(fl.open(dir));
-        //$LASTPOS=3000397;//jseditor.FileMenu:397
+        //$LASTPOS=3000438;//jseditor.FileMenu:438
         if (! isd) {
-          //$LASTPOS=3000417;//jseditor.FileMenu:417
+          //$LASTPOS=3000458;//jseditor.FileMenu:458
           es.open(nf);
           
         }
@@ -95,16 +97,16 @@ define(function (require) {
         var nf;
         var isd;
         
-        //$LASTPOS=3000074;//jseditor.FileMenu:74
+        //$LASTPOS=3000090;//jseditor.FileMenu:90
         es = _this.editorSet;
         
-        //$LASTPOS=3000096;//jseditor.FileMenu:96
+        //$LASTPOS=3000112;//jseditor.FileMenu:112
         fl = _this.fileList;
         
-        //$LASTPOS=3000117;//jseditor.FileMenu:117
+        //$LASTPOS=3000133;//jseditor.FileMenu:133
         dir = fl.curDir;
         
-        //$LASTPOS=3000140;//jseditor.FileMenu:140
+        //$LASTPOS=3000156;//jseditor.FileMenu:156
         if (! dir) {
           _thread.retVal=_this;return;
           
@@ -115,47 +117,49 @@ define(function (require) {
           for(var __cnt=100 ; __cnt--;) {
             switch (__pc) {
             case 0:
-              //$LASTPOS=3000162;//jseditor.FileMenu:162
+              //$LASTPOS=3000178;//jseditor.FileMenu:178
               _this.fiber$waitFor(_thread, UIDiag.prompt("ファイル名"));
               __pc=1;return;
             case 1:
               n=_thread.retVal;
               
-              //$LASTPOS=3000205;//jseditor.FileMenu:205
+              //$LASTPOS=3000221;//jseditor.FileMenu:221
+              console.log("fn",n);
+              //$LASTPOS=3000246;//jseditor.FileMenu:246
               nf = dir.rel(n);
               
-              //$LASTPOS=3000228;//jseditor.FileMenu:228
+              //$LASTPOS=3000269;//jseditor.FileMenu:269
               _this.fiber$waitFor(_thread, nf.isDir());
               __pc=2;return;
             case 2:
               isd=_thread.retVal;
               
-              //$LASTPOS=3000261;//jseditor.FileMenu:261
+              //$LASTPOS=3000302;//jseditor.FileMenu:302
               if (!(isd)) { __pc=4; break; }
-              //$LASTPOS=3000280;//jseditor.FileMenu:280
+              //$LASTPOS=3000321;//jseditor.FileMenu:321
               dir=nf;
-              //$LASTPOS=3000296;//jseditor.FileMenu:296
+              //$LASTPOS=3000337;//jseditor.FileMenu:337
               _this.fiber$waitFor(_thread, nf.mkdir());
               __pc=3;return;
             case 3:
               
               __pc=6;break;
             case 4:
-              //$LASTPOS=3000338;//jseditor.FileMenu:338
+              //$LASTPOS=3000379;//jseditor.FileMenu:379
               _this.fiber$waitFor(_thread, nf.text(""));
               __pc=5;return;
             case 5:
               
             case 6:
               
-              //$LASTPOS=3000370;//jseditor.FileMenu:370
+              //$LASTPOS=3000411;//jseditor.FileMenu:411
               _this.fiber$waitFor(_thread, fl.open(dir));
               __pc=7;return;
             case 7:
               
-              //$LASTPOS=3000397;//jseditor.FileMenu:397
+              //$LASTPOS=3000438;//jseditor.FileMenu:438
               if (! isd) {
-                //$LASTPOS=3000417;//jseditor.FileMenu:417
+                //$LASTPOS=3000458;//jseditor.FileMenu:458
                 es.open(nf);
                 
               }

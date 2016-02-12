e87225b75bd58b3dc46604deaa5e47a9f02aa6da
hoge1e3 <hoge1e3@gmail.com>
2016-02-12 16:48:47 +0900

    Test del

--- www/js/editor/FileMenu.js
+++ www/js/editor/FileMenu.js
@@ -1,6 +1,5 @@
 define(function (require) {
   var Tonyu=require('Tonyu');
-  var UIDiag=require('UIDiag');
   var Base=require('Base');
   return Tonyu.klass.define({
     fullName: 'jseditor.FileMenu',
@@ -33,52 +32,50 @@ define(function (require) {
         var nf;
         var isd;
         
-        //$LASTPOS=4000047;//jseditor.FileMenu:47
+        //$LASTPOS=5000047;//jseditor.FileMenu:47
         es = _this.editorSet;
         
-        //$LASTPOS=4000069;//jseditor.FileMenu:69
+        //$LASTPOS=5000069;//jseditor.FileMenu:69
         fl = _this.fileList;
         
-        //$LASTPOS=4000090;//jseditor.FileMenu:90
-        dir = fl.curDir;
+        //$LASTPOS=5000090;//jseditor.FileMenu:90
+        dir = _this.curDir;
         
-        //$LASTPOS=4000113;//jseditor.FileMenu:113
+        //$LASTPOS=5000110;//jseditor.FileMenu:110
         if (! dir) {
           return _this;
         }
-        //$LASTPOS=4000135;//jseditor.FileMenu:135
-        n = _this.waitFor(UIDiag.prompt("ファイル名"));
+        //$LASTPOS=5000132;//jseditor.FileMenu:132
+        n = _this.prompt("ファイル名");
         
-        //$LASTPOS=4000178;//jseditor.FileMenu:178
+        //$LASTPOS=5000159;//jseditor.FileMenu:159
         if (! n) {
           return _this;
         }
-        //$LASTPOS=4000198;//jseditor.FileMenu:198
-        console.log("fn",n);
-        //$LASTPOS=4000223;//jseditor.FileMenu:223
+        //$LASTPOS=5000179;//jseditor.FileMenu:179
         nf = dir.rel(n);
         
-        //$LASTPOS=4000246;//jseditor.FileMenu:246
-        isd = _this.waitFor(nf.isDir());
+        //$LASTPOS=5000202;//jseditor.FileMenu:202
+        isd = _this.isDir(nf);
         
-        //$LASTPOS=4000279;//jseditor.FileMenu:279
+        //$LASTPOS=5000225;//jseditor.FileMenu:225
         if (isd) {
-          //$LASTPOS=4000298;//jseditor.FileMenu:298
+          //$LASTPOS=5000244;//jseditor.FileMenu:244
           dir=nf;
-          //$LASTPOS=4000314;//jseditor.FileMenu:314
-          _this.waitFor(nf.mkdir());
+          //$LASTPOS=5000260;//jseditor.FileMenu:260
+          _this.mkdir(nf);
           
         } else {
-          //$LASTPOS=4000356;//jseditor.FileMenu:356
-          _this.waitFor(nf.text(""));
+          //$LASTPOS=5000292;//jseditor.FileMenu:292
+          _this.writeFile(nf,"");
           
         }
-        //$LASTPOS=4000388;//jseditor.FileMenu:388
+        //$LASTPOS=5000320;//jseditor.FileMenu:320
         _this.waitFor(fl.open(dir));
-        //$LASTPOS=4000415;//jseditor.FileMenu:415
+        //$LASTPOS=5000347;//jseditor.FileMenu:347
         if (! isd) {
-          //$LASTPOS=4000435;//jseditor.FileMenu:435
-          es.open(nf);
+          //$LASTPOS=5000367;//jseditor.FileMenu:367
+          _this.waitFor(fl.open(nf));
           
         }
       },
@@ -94,16 +91,16 @@ define(function (require) {
         var nf;
         var isd;
         
-        //$LASTPOS=4000047;//jseditor.FileMenu:47
+        //$LASTPOS=5000047;//jseditor.FileMenu:47
         es = _this.editorSet;
         
-        //$LASTPOS=4000069;//jseditor.FileMenu:69
+        //$LASTPOS=5000069;//jseditor.FileMenu:69
         fl = _this.fileList;
         
-        //$LASTPOS=4000090;//jseditor.FileMenu:90
-        dir = fl.curDir;
+        //$LASTPOS=5000090;//jseditor.FileMenu:90
+        dir = _this.curDir;
         
-        //$LASTPOS=4000113;//jseditor.FileMenu:113
+        //$LASTPOS=5000110;//jseditor.FileMenu:110
         if (! dir) {
           _thread.retVal=_this;return;
           
@@ -114,57 +111,58 @@ define(function (require) {
           for(var __cnt=100 ; __cnt--;) {
             switch (__pc) {
             case 0:
-              //$LASTPOS=4000135;//jseditor.FileMenu:135
-              _this.fiber$waitFor(_thread, UIDiag.prompt("ファイル名"));
+              //$LASTPOS=5000132;//jseditor.FileMenu:132
+              _this.fiber$prompt(_thread, "ファイル名");
               __pc=1;return;
             case 1:
               n=_thread.retVal;
               
-              //$LASTPOS=4000178;//jseditor.FileMenu:178
+              //$LASTPOS=5000159;//jseditor.FileMenu:159
               if (!(! n)) { __pc=2; break; }
               _thread.exit(_this);return;
             case 2:
               
-              //$LASTPOS=4000198;//jseditor.FileMenu:198
-              console.log("fn",n);
-              //$LASTPOS=4000223;//jseditor.FileMenu:223
+              //$LASTPOS=5000179;//jseditor.FileMenu:179
               nf = dir.rel(n);
               
-              //$LASTPOS=4000246;//jseditor.FileMenu:246
-              _this.fiber$waitFor(_thread, nf.isDir());
+              //$LASTPOS=5000202;//jseditor.FileMenu:202
+              _this.fiber$isDir(_thread, nf);
               __pc=3;return;
             case 3:
               isd=_thread.retVal;
               
-              //$LASTPOS=4000279;//jseditor.FileMenu:279
+              //$LASTPOS=5000225;//jseditor.FileMenu:225
               if (!(isd)) { __pc=5; break; }
-              //$LASTPOS=4000298;//jseditor.FileMenu:298
+              //$LASTPOS=5000244;//jseditor.FileMenu:244
               dir=nf;
-              //$LASTPOS=4000314;//jseditor.FileMenu:314
-              _this.fiber$waitFor(_thread, nf.mkdir());
+              //$LASTPOS=5000260;//jseditor.FileMenu:260
+              _this.fiber$mkdir(_thread, nf);
               __pc=4;return;
             case 4:
               
               __pc=7;break;
             case 5:
-              //$LASTPOS=4000356;//jseditor.FileMenu:356
-              _this.fiber$waitFor(_thread, nf.text(""));
+              //$LASTPOS=5000292;//jseditor.FileMenu:292
+              _this.fiber$writeFile(_thread, nf, "");
               __pc=6;return;
             case 6:
               
             case 7:
               
-              //$LASTPOS=4000388;//jseditor.FileMenu:388
+              //$LASTPOS=5000320;//jseditor.FileMenu:320
               _this.fiber$waitFor(_thread, fl.open(dir));
               __pc=8;return;
             case 8:
               
-              //$LASTPOS=4000415;//jseditor.FileMenu:415
-              if (! isd) {
-                //$LASTPOS=4000435;//jseditor.FileMenu:435
-                es.open(nf);
-                
-              }
+              //$LASTPOS=5000347;//jseditor.FileMenu:347
+              if (!(! isd)) { __pc=10; break; }
+              //$LASTPOS=5000367;//jseditor.FileMenu:367
+              _this.fiber$waitFor(_thread, fl.open(nf));
+              __pc=9;return;
+            case 9:
+              
+            case 10:
+              
               _thread.exit(_this);return;
             }
           }
@@ -198,22 +196,106 @@ define(function (require) {
         
         _thread.retVal=_this;return;
       },
-      del :function _trc_FileMenu_del() {
+      __getter__curDir :function _trc_FileMenu___getter__curDir() {
         "use strict";
         var _this=this;
         
+        return _this.fileList.curDir;
       },
-      fiber$del :function _trc_FileMenu_f_del(_thread) {
+      remove :function _trc_FileMenu_remove() {
+        "use strict";
+        var _this=this;
+        var fl;
+        var f;
+        var r;
+        
+        //$LASTPOS=5000490;//jseditor.FileMenu:490
+        fl = _this.fileList;
+        
+        //$LASTPOS=5000511;//jseditor.FileMenu:511
+        f = fl.lastSelected;
+        
+        //$LASTPOS=5000538;//jseditor.FileMenu:538
+        if (! f) {
+          return _this;
+        }
+        //$LASTPOS=5000558;//jseditor.FileMenu:558
+        r = _this.confirm(f.path()+"を削除しますか？");
+        
+        //$LASTPOS=5000598;//jseditor.FileMenu:598
+        if (! r) {
+          return _this;
+        }
+        //$LASTPOS=5000618;//jseditor.FileMenu:618
+        _this.rm(f);
+        //$LASTPOS=5000629;//jseditor.FileMenu:629
+        _this.editorSet.close(f);
+        //$LASTPOS=5000653;//jseditor.FileMenu:653
+        if (_this.curDir) {
+          //$LASTPOS=5000665;//jseditor.FileMenu:665
+          _this.waitFor(fl.open(_this.curDir));
+        }
+      },
+      fiber$remove :function _trc_FileMenu_f_remove(_thread) {
         "use strict";
         var _this=this;
         //var _arguments=Tonyu.A(arguments);
         var __pc=0;
+        var fl;
+        var f;
+        var r;
         
+        //$LASTPOS=5000490;//jseditor.FileMenu:490
+        fl = _this.fileList;
         
-        _thread.retVal=_this;return;
+        //$LASTPOS=5000511;//jseditor.FileMenu:511
+        f = fl.lastSelected;
+        
+        //$LASTPOS=5000538;//jseditor.FileMenu:538
+        if (! f) {
+          _thread.retVal=_this;return;
+          
+        }
+        
+        _thread.enter(function _trc_FileMenu_ent_remove(_thread) {
+          if (_thread.lastEx) __pc=_thread.catchPC;
+          for(var __cnt=100 ; __cnt--;) {
+            switch (__pc) {
+            case 0:
+              //$LASTPOS=5000558;//jseditor.FileMenu:558
+              _this.fiber$confirm(_thread, f.path()+"を削除しますか？");
+              __pc=1;return;
+            case 1:
+              r=_thread.retVal;
+              
+              //$LASTPOS=5000598;//jseditor.FileMenu:598
+              if (!(! r)) { __pc=2; break; }
+              _thread.exit(_this);return;
+            case 2:
+              
+              //$LASTPOS=5000618;//jseditor.FileMenu:618
+              _this.fiber$rm(_thread, f);
+              __pc=3;return;
+            case 3:
+              
+              //$LASTPOS=5000629;//jseditor.FileMenu:629
+              _this.editorSet.close(f);
+              //$LASTPOS=5000653;//jseditor.FileMenu:653
+              if (!(_this.curDir)) { __pc=5; break; }
+              //$LASTPOS=5000665;//jseditor.FileMenu:665
+              _this.fiber$waitFor(_thread, fl.open(_this.curDir));
+              __pc=4;return;
+            case 4:
+              
+            case 5:
+              
+              _thread.exit(_this);return;
+            }
+          }
+        });
       },
       __dummy: false
     },
-    decls: {"methods":{"main":{"nowait":false},"create":{"nowait":false},"rename":{"nowait":false},"copy":{"nowait":false},"del":{"nowait":false}}}
+    decls: {"methods":{"main":{"nowait":false},"create":{"nowait":false},"rename":{"nowait":false},"copy":{"nowait":false},"__getter__curDir":{"nowait":true},"remove":{"nowait":false}}}
   });
 });

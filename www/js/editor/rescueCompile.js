10957fe85825c6d4cb19934857d8f99adcc761e7
hoge1e3 <hoge1e3@gmail.com>
2016-05-22 12:05:26 +0900

    rescue bug

--- www/js/editor/rescueCompile.js
+++ www/js/editor/rescueCompile.js
@@ -2,7 +2,7 @@ define(function (require, exports, module) {
     var TonyuC=require("TonyuC");
     var FS=require("FS");
     var JSEProject=require("JSEProject");
-    var prj=new JSEProject({}, "jseditor");
+    var prj=new JSEProject([], "jseditor");
     var tc=new TonyuC(prj);
     tc.parallel("compile");
 });
